a2d754616a87efc129e873ccde34d7a52e5b2743
Adelaide <adiainthesky@gmail.com>
2021-07-27 19:51:49 -0700

    add pins located using external data, with embedded pop-ups

--- src/App.js
+++ src/App.js
@@ -1,17 +1,67 @@
 import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import * as fauxData from "./data/fauxData.json";
+// import * as parkData from "./data/fauxData.json";
 import './App.css';
 
-function App() {
+export const thumbtack = new Icon({
+  iconUrl: "https://img.icons8.com/emoji/48/000000/pushpin-emoji.png",
+  iconSize: [25, 25]
+});
+
+export default function App() {
+  const [activeLocation, setActiveLocation] = React.useState(null);
+
   return (
-    <MapContainer center={[45.4, -75.7]} zoom={12}>
+    <MapContainer center={[45.52, -122.67]} zoom={12}>
       <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> 
           contributors'
         />
+
+        {fauxData.features.map(location => (
+          <Marker
+            key={location.properties.PARK_ID}
+            position={[
+              //their lat/long is in reverse order so below the [1] comes before the [0]
+              location.geometry.coordinates[1],
+              location.geometry.coordinates[0]
+            ]}
+            //  //apparently this is not needed, as the marker handles it automatically!
+            // onClick={() => {
+            //   setActiveLocation(location);
+            //   console.log({location})
+            // }}
+            // icon={icon}
+          >
+          <Popup
+        //like a location, popups need to know their location
+          position={[
+            location.geometry.coordinates[1],
+            location.geometry.coordinates[0]
+          ]}
+
+          //  //apparently this is not needed, as the marker handles it automatically!
+          // onClose={() => {
+          //   setActiveLocation(null);
+          // }}
+        >
+          <div> 
+            {/* info in pop-up */}
+            <h2>{location.properties.NAME}</h2>
+            <p>{location.properties.DESCRIPTIO}</p>
+          </div>
+        </Popup>
+            </Marker>
+      ))}
+
     </MapContainer>
   );  
 }
 
-export default App;
+
+
+
+
