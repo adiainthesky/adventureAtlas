83d2d5d4e6db92130685220a1eb6d17b6036013a
Adelaide <adiainthesky@gmail.com>
2021-08-10 00:38:17 -0700

    clean up app.js file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import React from "react";
-import { Icon } from "leaflet";
-// import * as fauxData from "./data/fauxData.json";
-// import * as parkData from "./data/fauxData.json";
 import './App.css';
 import Map from "./comps/Map";
 
-
-export const thumbtack = new Icon({
-  iconUrl: "/simple-luggage.svg",
-  iconSize: [35, 35]
-});
 
 export default function App() {
 
