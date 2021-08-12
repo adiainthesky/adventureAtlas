534d232bce31385abbd9c6ccffd986f46166e18c
Adelaide <adiainthesky@gmail.com>
2021-08-12 10:14:19 -0700

    new trip pins stay rendered on map

--- src/hooks/useFirestore.jsx
+++ src/hooks/useFirestore.jsx
@@ -1,36 +1,37 @@
-import { UnsupportedMediaType } from 'http-errors';
-import React, { useState, useEffect } from 'react';
+// import { UnsupportedMediaType } from 'http-errors';
+import { useState, useEffect } from 'react';
 import { db } from '../firebase/config';
 
 const useFirestore = (collection) => {
-    const [docs, setDocs] =useState([]);
+    const [docs, setDocs] = useState([]);
 
     useEffect(() => {
         // unsub = unsubscribe => to stop listening to this collection (ie, if we want to unmount from imgGrid)
         // const unsub = db.collection(collection)
-        //     .orderBy('createdAt', 'desc')
-        //     .onSnapshot((snap) => {
-        //         let documents = [];
-        //         console.log("^^^^^^^^^^^^^^^^^^^^^^")
-        //         snap.forEach(doc => {
-        //             console.log("$$$$$$$$$$$$$$$$$$$$$$$$")
-        //             documents.push({...doc.data(), id: doc.id});
-        //         });
-        //         setDocs(documents);
-        //     })
-
-
-        const fetchLoctions=async()=>{
-            const response=db.collection(collection);
-            const data=await response.get();
-            data.docs.forEach(item=>{
-            setDocs([...docs,item.data()])
+        db.collection(collection)
+            // .orderBy('createdAt', 'desc')
+            .onSnapshot((snap) => {
+                let documents = [];
+                console.log("^^^^^^^^^^^^^^^^^^^^^^")
+                snap.forEach(doc => {
+                    console.log("$$$$$$$$$$$$$$$$$$$$$$$$")
+                    documents.push({...doc.data(), id: doc.id});
+                });
+                setDocs(documents);
             })
-        }    
-        fetchLoctions()
-
         // this is a 'clean-up method' (unsubscribing from teh collection when we no longer need it)    
         // return () => unsub();
+
+        // const fetchLoctions=async()=>{
+        //     const response=db.collection(collection);
+        //     const data=await response.get();
+        //     data.docs.forEach(item=>{
+        //     setDocs([...docs,item.data()])
+        //     })
+        // }    
+        // fetchLoctions()
+
+
 
     }, [collection])
 
