48603cc1dafb18882f156ceff886d742ea89bdac
Adelaide <adiainthesky@gmail.com>
2021-08-18 13:57:27 -0700

    add copy and CSS edits to sidebar

--- src/comps/sidebar/Sidebar.jsx
+++ src/comps/sidebar/Sidebar.jsx
@@ -10,6 +10,7 @@ import './Sidebar.css'
 import { IconCentext } from 'react-icons'
 import { IconContext } from 'react-icons/lib';
 import InfoModal from './InfoModal';
+import { luggage, plant, tea } from '../map/Map';
 
 
 const Sidebar = ({activateModal}) => {
@@ -31,14 +32,34 @@ const Sidebar = ({activateModal}) => {
                     <div className='side-menu-items' onClick={toggleInfo}>
                     {/* <div className='side-menu-items'> */}
                         <div className="closer">
-                            <div className='menu-bars'>
                                 {/* got below icon from 'https://react-icons.github.io/' then imported above' */}
                                 {/* <CgIcons.CgCloseR classname='sideBarCloser'/> */}
-                                <CgIcons.CgCloseR size={15} color={'darkgreen'} margin={0}/>
-                            </div>
+                                <CgIcons.CgCloseR size={15} color={'#f3a33'} margin={0}/>
                         </div>
-                        <p className="aboutText">Flee in terror at cucumber discovered on floor gnaw the corn cob. Man running from cops stops to pet cats, goes to jail prance along on top of the garden fence, annoy the neighbor's dog and make it bark rub butt on table or meow meow mama. Steal the warm chair right after you get up walk on keyboard or twitch tail in permanent irritation scratch the box rub whiskers on bare skin act innocent but hate dogs. Kitty loves pigs meowing chowing and wowing. Scratch so owner bleeds purr purr purr until owner pets why owner not pet me hiss scratch meow yet meow loudly just to annoy owners. Mark territory under the bed cats are a queer kind of folk. Weigh eight pounds but take up a full-size bed get suspicious of own shadow then go play with toilette paper yet hate dogs get scared by doggo also cucumerro , and meow all night steal mom's crouton while she is in the bathroom so favor packaging over toy. Do i like standing on litter cuz i sits when i have spaces, my cat buddies have no litter i live in luxury cat life steal mom's crouton while she is in the bathroom instead of drinking water from the cat bowl, make sure to steal water from the toilet and rub butt on table yet purr while eating. Cat mojo chase laser but walk on a keyboard and why must they do that. Run at 3am then cats take over the world. Kitty kitty roll over and sun my belly but groom yourself 4 hours - checked, have your beauty sleep 18 hours - checked, be fabulous for the rest of the day - checked. Pet right here, no not there, here, no fool, right here that other cat smells funny you should really give me all the treats because i smell the best and omg you finally got the right spot and i love you right now eat my own ears and nap all day, yet jump around on couch, meow constantly until given food, , for scratch at the door then walk away.
-</p>
+                        <text className="sideTextTitle">Welcome to Adventure Atlas, a place where we share visual and written snapshots taken from explorations around our amazing planet!</text>  
+                        <br />
+                        <text className="sideText">
+                        Spend some time clicking on the map icons to learn more about fellow traveler’s tales. 
+                        </text>
+                        <text className="sideTextIcon">
+                        <br />
+                        ☕ = “cultural” experiences.  These could range anywhere from dancing to Vallenato on the beach in Baranquilla, Colombia, to sipping chai in a hip cafe in Mumbai, to visiting the ruins of Angkor Wat.
+                        <br />
+                        🌴 = “nature” experience.  These include things like kayaking trips down the Colorado river, volunteering with injured manatees in Belize, or having a picnic in the forest in Bulgaria.  
+                        <br />
+                        🧳 = general, uncategorized experiences and can represent the gamut of travel adventures. 
+                        <br />
+                        <br />
+                        </text>
+                        <text className="sideText">
+                        If you would like to share some travels of your own, please signup and login at the bottom left! 
+                        <br />
+                        </text>
+                        <text className="sideTextTitle">
+                        <br />
+                        Happy virtual travels! 
+                        </text>
+                        
                         {/* {SidebarData.map((item, index) => {
                             return (
                                 <li key={index} className={item.className} onClick={activateModal}>
